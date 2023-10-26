52842e8f2bca456eab6c9f03e36c7bc6a9225b99
Irael99XD <israelsldn@gmail.com>
2023-10-25 21:59:07 -0600

    Segunda subida

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -52,7 +52,7 @@ export const createNewNote = async (req, res) => {
   }
 
   const newNote = new Note({
-    
+
     nombre,
     descripcion: tareas,
     receptor,
@@ -84,65 +84,52 @@ export const renderNotes = async (req, res) => {
 export const renderEditForm = async (req, res) => {
   const note = await Note.findById(req.params.id).lean();
   const piezas = await Pieza.find({}).lean(); // Obtener todas las piezas
-  const tareas = await Note.find({},{descripcion: 1}).lean();
-  res.render("notes/edit-note", { note, piezas, tareas});
+  const tareas = await Note.find({}, { descripcion: 1 }).lean();
+  res.render("notes/edit-note", { note, piezas, tareas });
 };
 
 
 export const updateNote = async (req, res) => {
   const {
-    nombre,
-    nServicio,
-    descripcion,
-    receptor,
-    contrasenaEquipo,
-    respaldo,
-    passEquipo,
-    malEquipo,
-    descripcionDanio,
     status,
-    presupuesto,
-    nTelefono,
-    email,
-    piezas,
-    necesitaPiezas // Piezas seleccionadas como un array
+    piezas,// Piezas seleccionadas como un array
   } = req.body;
 
   const noteId = req.params.id;
 
-  await Note.findByIdAndUpdate(noteId, {
-    nombre,
-    descripcion,
-    receptor,
-    contrasenaEquipo,
-    respaldo,
-    passEquipo,
-    malEquipo,
-    descripcionDanio,
-    status,
-    nServicio,
-    presupuesto,
-    nTelefono,
-    email,
-    piezas,
-    necesitaPiezas // Actualiza las piezas seleccionadas en la orden
-  });
-  if (piezas) {
+ 
+  const selectedPieza = await Pieza.findById(piezas);
+
+
+  if (selectedPieza) {
     try {
-      const pieza = await Pieza.findOne({nombre: piezas});
-      console.log(pieza.cantidad);
-      if (pieza && pieza.cantidad > 0) {
-        pieza.cantidad -= 1;
-        await pieza.save();
+      if (selectedPieza.cantidad > 0) {
+        // Crear un objeto que represente la pieza seleccionada
+        const piezaSeleccionada = {
+          id: selectedPieza._id,
+          nombre: selectedPieza.nombre,
+          precio: selectedPieza.precio
+        };
+
+        const noteOLD = await Note.findById(noteId);
+        noteOLD.piezas.push(piezaSeleccionada);
+        noteOLD.status = status;
+        await noteOLD.save()
+
+        selectedPieza.cantidad -= 1;
+        await selectedPieza.save();
       } else {
         req.flash("error_msg", "No hay suficientes piezas disponibles");
       }
+      
     } catch (error) {
       console.error(error);
       req.flash("error_msg", "Error al actualizar la cantidad de la pieza");
     }
   }
+  req.flash("success_msg", "Orden actualizada correctamente");
   res.redirect("/notes");
+
 };
 
 
